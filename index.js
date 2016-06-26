ebafd5e1041296721add0356cc4f8713a7f901dc
Eric Scouten <eric@scouten.com>
2016-06-26 15:35:39 -0700

    More logging. This suggests that the 'b_' items are not getting processed at all.

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 var Rx = require('rx');
 
+var obsCount = 0;
+
 var source = Rx.Observable.from(['a1', 'a2', 'b1', 'b2', 'a3', 'a4', 'b3', 'b4'])
   .groupBy(function (item) { return item.substr(0, 1); })
+  .map(function (obs) {
+    var seq = ++obsCount;
+    console.log('\n\n\nNEW GROUP %d\n\n\n', seq);
+    return obs.tap(function (item) {
+      console.log('processing item in group %d: %s', seq, item);
+    });
+  })
   .concatAll();
 
 var subscription = source.subscribe(
