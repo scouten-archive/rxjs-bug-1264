face4cff871395b63adf990e2b2387d338195034
Eric Scouten <eric@scouten.com>
2016-06-26 15:01:56 -0700

    Repro demo case from RxJS docs.

---
+++ index.js
@@ -0,0 +1,16 @@
+var Rx = require('rx');
+
+var source = Rx.Observable.range(0, 3)
+  .map(function (x) { return Rx.Observable.range(x, 3); })
+  .concatAll();
+
+var subscription = source.subscribe(
+  function (x) {
+    console.log('Next: %s', x);
+  },
+  function (err) {
+    console.log('Error: %s', err);
+  },
+  function () {
+    console.log('Completed');
+  });
