75f8b5472c5b5fc4cd911096e583f635a81d5023
Eric Scouten <eric@scouten.com>
2016-06-26 16:15:27 -0700

    Uff da. Hot vs cold observables FTL.

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ var Rx = require('rx');
 
 var source = Rx.Observable.from(['a1', 'a2', 'b1', 'b2', 'a3', 'a4', 'b3', 'b4'])
   .groupBy(function (item) { return item.substr(0, 1); })
+  .map(function (obs) {
+    var result = obs.replay();
+    result.connect();
+    return result;
+  })
   .concatAll();
 
 var subscription = source.subscribe(
